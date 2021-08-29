1cbad299d6104d836abc715e807233882dfe7757
Carlos Balcazar <cbalcazarp@gmail.com>
2021-08-28 20:06:50 -0500

    added Interes - writeline

--- Program.cs
+++ Program.cs
@@ -8,13 +8,19 @@ namespace upn_T1_q_8
         {
             Double capital, tasaInteres, periodo, montoDevolver, Interes;
             Console.WriteLine("Calculo de interés simple");
+            
             Console.Write("Ingrese capital (soles): ");
             capital = Convert.ToDouble(Console.ReadLine());
+            
             Console.Write("Ingrese tasaInteres simple anual (%): ");
             tasaInteres = Convert.ToDouble(Console.ReadLine());
+            
             Console.Write("Ingrese período (meses): ");
             periodo = Convert.ToDouble(Console.ReadLine());
+            
             Interes = capital * (tasaInteres/100) * (periodo/12);
+            Console.Write("Interes es: " + Convert.ToString(Interes) + " soles");
+
             montoDevolver = capital + Interes;
             Console.WriteLine("El monto a devolver es: " + Convert.ToString(montoDevolver) + " soles");
 
