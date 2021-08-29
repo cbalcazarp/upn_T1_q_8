318e1c5fd43ccd2a73718fe17969a1dc5488b48f
Carlos Balcazar <cbalcazarp@gmail.com>
2021-08-28 19:51:13 -0500

    code added

--- Program.cs
+++ Program.cs
@@ -6,7 +6,18 @@ namespace upn_T1_q_8
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Double capital, tasaInteres, periodo, montoDevolver;
+            Console.WriteLine("Calculo de interés simple");
+            Console.Write("Ingrese capital (soles): ");
+            capital = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Ingrese tasaInteres (%): ");
+            tasaInteres = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Ingrese período (meses): ");
+            periodo = Convert.ToDouble(Console.ReadLine());
+            tasaInteres = capital*tasaInteres*periodo/1200;
+            montoDevolver = capital + tasaInteres;
+            Console.WriteLine("El monto a devolver es: " + Convert.ToString(montoDevolver) + " soles");
+
         }
     }
 }
